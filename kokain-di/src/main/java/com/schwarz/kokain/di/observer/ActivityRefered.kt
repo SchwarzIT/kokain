d036b1f71945063b9183af44227e6c96db2c31ff
Sven Braun <sven.braune@mail.schwarz>
2021-06-08 15:22:53 +0200

    # fixed test setup # restructured api relevant files

--- kokain-di/src/main/java/com/schwarz/kokain/di/observer/ActivityRefered.kt
+++
@@ -1,7 +0,0 @@
-package com.schwarz.kokain.di.observer
-
-interface ActivityRefered {
-
-    var activityRef : String?
-
-}
--- kokain-di/src/main/java/com/schwarz/kokain/di/scope/BeanScope.kt
+++
@@ -1,8 +0,0 @@
-package com.schwarz.kokain.di.scope
-
-import com.schwarz.kokain.api.EBean
-
-interface BeanScope {
-
-    var scope : EBean.Scope
-}
