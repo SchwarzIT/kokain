10319de6e54dfdf599b12c35a7c92354d16a1a8c
Sven Braune <sven.braune@mail.schwarz>
2022-10-21 12:02:52 +0200

    # bump ksp to 1.0.7 # added internal bean to demo project # updated kotlin & gradle

---
+++ demo/src/main/java/kaufland/com/demo/bean/FooInternalBean.kt
@@ -0,0 +1,23 @@
+package kaufland.com.demo.bean
+
+import android.app.Application
+import android.content.Context
+import com.schwarz.kokain.api.EBean
+import com.schwarz.kokain.di.context
+import java.lang.RuntimeException
+
+@EBean
+internal class FooInternalBean {
+
+    val context: Context by context()
+
+    val startTime = System.currentTimeMillis()
+
+    fun calculateRunningTime(): Long {
+        if (!(context is Application)) {
+            throw RuntimeException("That should not happen in a SingletonBean")
+        }
+
+        return System.currentTimeMillis().minus(startTime)
+    }
+}
